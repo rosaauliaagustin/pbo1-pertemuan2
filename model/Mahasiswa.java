347416e5affe874da1089836f70ca696949dd1b7
rosaauliaagustin <rosaauliaagustin22@gmail.com>
2020-03-25 11:45:29 +0700

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ======================");
+  System.out.println("NPM  : 18630804");
+  System.out.println("NAMA : Rosa Aulia Agustin ");
+  System.out.println("========================================");
+ }
+}
